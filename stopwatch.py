08db0867b8d64145ccb091e0decc9f347afad765
Nguyen Tu Nhu <129203784+YvesFleur@users.noreply.github.com>
2023-03-31 20:35:22 +0700

    Update stopwatch.py

--- stopwatch.py
+++ stopwatch.py
@@ -7,16 +7,16 @@ root = Tk()
 root.title("Stopwatch")
 root.minsize(width=250, height=70)
 
-s = 0
-m = 0
-h = 0
+s = 0   #second value
+m = 0   #minute value
+h = 0   #hour value
 runnning = False
 
 def counter():
     global h,m,s, begin, end
+    
     if running:
-
-        begin = time()
+        begin = time()  #time in second when pressing Start button
         s += 1
         if s == 60:
             s = s % 60
@@ -26,9 +26,12 @@ def counter():
             h += 1
         disp = datetime.time(h, m, s)
         display.config(text=disp)
-        end = time()
-    display.after(round(1000-(end - begin)*1000), counter)
-
+        end = time()    #time in second after executing values of s, m, h
+        time_taken = end - begin    #duration for executing values of s, m, h
+        
+    #to make sure total is 1000ms
+    display.after(round(1000-time_taken*1000), counter) 
+    
 def start():
     global running
     running = True
